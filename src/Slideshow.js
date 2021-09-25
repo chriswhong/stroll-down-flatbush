4156fc2b8e3ebab39969f6bab6c51e1f58a36c47
Chris Whong <chris.m.whong@gmail.com>
2021-09-24 22:39:10 -0400

    lazy load images

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -5,99 +5,6 @@ import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 import 'react-slideshow-image/dist/styles.css'
 
-
-//
-// const images = [
-//   '/images/53+50.jpg',
-//   '/images/52+95.jpg',
-//   '/images/52+53.jpg',
-//   '/images/52+00.jpg',
-//   '/images/51+10.jpg',
-//   '/images/50+65.jpg',
-//   '/images/50+15.jpg',
-//   '/images/49+65.jpg',
-//   '/images/49+15.jpg',
-//   '/images/48+65.jpg',
-//   '/images/48+15.jpg',
-//   '/images/47+65.jpg',
-//   '/images/46+65.jpg',
-//   '/images/46+15.jpg',
-//   '/images/45+65.jpg',
-//   '/images/45+15.jpg',
-//   '/images/44+50.jpg',
-//   '/images/44+12.jpg',
-//   '/images/43+65.jpg',
-//   '/images/43+15.jpg',
-//   '/images/42+00.jpg',
-//   '/images/41+50.jpg',
-//   '/images/41+00.jpg',
-//   '/images/40+50.jpg',
-//   '/images/40+00.jpg',
-//   '/images/39+50.jpg',
-//   '/images/38+50.jpg',
-//   '/images/37+50.jpg',
-//   '/images/37+00.jpg',
-//   '/images/36+65.jpg',
-//   '/images/36+50.jpg',
-//   '/images/36+15.jpg',
-//   '/images/35+15.jpg',
-//   '/images/34+65.jpg',
-//   '/images/34+15.jpg',
-//   '/images/33+50.jpg',
-//   '/images/33+00.jpg',
-//   '/images/32+50.jpg',
-//   '/images/32+00.jpg',
-//   '/images/31+50.jpg',
-//   '/images/31+00.jpg',
-//   '/images/29+80.jpg',
-//   '/images/29+15.jpg',
-//   '/images/28+65.jpg',
-//   '/images/28+15.jpg',
-//   '/images/27+65.jpg',
-//   '/images/27+15.jpg',
-//   '/images/26+65.jpg',
-//   '/images/26+15.jpg',
-//   '/images/25+65.jpg',
-//   '/images/25+15.jpg',
-//   '/images/24+65.jpg',
-//   '/images/24+15.jpg',
-//   '/images/23+65.jpg',
-//   '/images/22+65.jpg',
-//   '/images/22+15.jpg',
-//   '/images/21+65.jpg',
-//   '/images/20+65.jpg',
-//   '/images/20+15.jpg',
-//   '/images/19+65.jpg',
-//   '/images/19+15.jpg',
-//   '/images/18+65.jpg',
-//   '/images/18+15.jpg',
-//   '/images/17+15.jpg'
-// ]
-
-// from example at https://github.com/femioladeji/react-slideshow/issues/59
-const lazyLoadImages = () => {
-  const lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-    if ("IntersectionObserver" in window) {
-      let lazyImageObserver = new IntersectionObserver(function(
-        entries,
-        observer
-      ) {
-        entries.forEach(function(entry) {
-          if (entry.isIntersecting) {
-            let lazyImage = entry.target;
-            lazyImage.src = lazyImage.dataset.src;
-            lazyImage.classList.remove("lazy");
-            lazyImageObserver.unobserve(lazyImage);
-          }
-        });
-      });
-
-      lazyImages.forEach(function(lazyImage) {
-        lazyImageObserver.observe(lazyImage);
-      });
-    }
-}
-
 class Slideshow extends Component {
   constructor() {
     super();
@@ -128,7 +35,6 @@ class Slideshow extends Component {
 
   componentDidMount() {
     this.containerRef.current.focus();
-    lazyLoadImages()
   }
 
   componentDidUpdate() {
@@ -150,6 +56,9 @@ class Slideshow extends Component {
 
   handleSlideChange(oldIndex, newIndex) {
     this.props.onIndexChange(newIndex)
+    // load index+1 image
+    const [nextImage] = [].slice.call(document.querySelectorAll(`img[data-index="${newIndex + 1}"]`));
+    nextImage.src = nextImage.dataset.src;
   }
 
   render() {
@@ -189,8 +98,9 @@ class Slideshow extends Component {
                     <img
                       className='lazy'
                       style={{ objectFit: "contain", width: "100%" }}
-                      src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+                      src={[0,1].includes(index) ? `/images/${feature.properties.marker}.jpg` : "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="}
                       data-src={`/images/${feature.properties.marker}.jpg`}
+                      data-index={index}
                       alt={`/images/${feature.properties.marker}.jpg`}
                     />
                   </div>
